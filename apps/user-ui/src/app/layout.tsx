f8e6be1d27a1b6b1cb6037b475899f15fbce58ce
Gomab <egoiomab@gmail.com>
2026-03-28 11:23:34 +0100

    feat: intergartion auth api back to front, header user menu, multi-session auth, logout, carrier schema

--- apps/user-ui/src/app/layout.tsx
+++ apps/user-ui/src/app/layout.tsx
@@ -4,6 +4,7 @@ import Header from "@/components/layout/Header";
 import { Plus_Jakarta_Sans } from "next/font/google";
 // import AppHeader from "@/components/layout/AppHeader";
 import { UiPreferencesProvider } from "@/components/providers/UiPreferencesProvider";
+import Providers from "@/app/providers";
 
 const plusJakarta = Plus_Jakarta_Sans({
   subsets: ["latin"],
@@ -11,30 +12,37 @@ const plusJakarta = Plus_Jakarta_Sans({
   display: "swap",
 });
 
+export const metadata = {
+  title: "Yamba",
+  description: "Yamba description"
+}
+
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="fr" className={plusJakarta.variable} suppressHydrationWarning>
     <body className="min-h-screen bg-white font-sans text-slate-900 dark:bg-slate-950 dark:text-slate-50">
-    {/*<style jsx global>{`*/}
-    {/*      @keyframes yambaShimmer {*/}
-    {/*        0% {*/}
-    {/*          transform: translateX(-100%);*/}
-    {/*        }*/}
-    {/*        100% {*/}
-    {/*          transform: translateX(100%);*/}
-    {/*        }*/}
-    {/*      }*/}
-    {/*    `}</style>*/}
-    <ThemeProvider>
-      <UiPreferencesProvider>
-        <Header />
-        {/*<AppHeader />*/}
-        <div className="pt-[78px]">
-          {children}
-        </div>
+      <Providers>
+        {/*<style jsx global>{`*/}
+        {/*      @keyframes yambaShimmer {*/}
+        {/*        0% {*/}
+        {/*          transform: translateX(-100%);*/}
+        {/*        }*/}
+        {/*        100% {*/}
+        {/*          transform: translateX(100%);*/}
+        {/*        }*/}
+        {/*      }*/}
+        {/*    `}</style>*/}
+        <ThemeProvider>
+          <UiPreferencesProvider>
+            <Header />
+            {/*<AppHeader />*/}
+            <div className="pt-[78px]">
+              {children}
+            </div>
 
-      </UiPreferencesProvider>
-    </ThemeProvider>
+          </UiPreferencesProvider>
+        </ThemeProvider>
+      </Providers>
     </body>
     </html>
   );
